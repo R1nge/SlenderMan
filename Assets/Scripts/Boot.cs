3e2831de8e58115227d9ad96fe28dc1116bd56ed
R1nge <r1ngegamedev@gmail.com>
2023-08-07 16:29:40 +0300

    Fixed scope parenting

--- Assets/Scripts/Boot.cs
+++ Assets/Scripts/Boot.cs
@@ -3,5 +3,5 @@ using UnityEngine.SceneManagement;
 
 public class Boot : MonoBehaviour
 {
-    private void Awake() => SceneManager.LoadSceneAsync("Game", LoadSceneMode.Single);
+    private void Start() => SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
 }
