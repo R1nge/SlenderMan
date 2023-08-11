49bfbf22da47ad1a75c71d7b0a0361676e952f82
R1nge <r1ngegamedev@gmail.com>
2023-08-11 18:59:09 +0300

    Human health UI

---
+++ Assets/Scripts/Characters/Human/UI/HealthUI.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Characters.Human.UI
+{
+    public class HealthUI : NetworkBehaviour
+    {
+        [SerializeField] private GameObject healthCanvas;
+        [SerializeField] private TextMeshProUGUI healthText;
+        private HumanHealthView _humanHealth;
+
+        private void Awake()
+        {
+            _humanHealth = GetComponent<HumanHealthView>();
+        }
+
+        private void Start()
+        {
+            healthCanvas.gameObject.SetActive(IsOwner);
+            if (!IsOwner) return;
+            _humanHealth.CurrentHealth.OnValueChanged += HealthChanged;
+            HealthChanged(0, _humanHealth.CurrentHealth.Value);
+        }
+
+        private void HealthChanged(int _, int health)
+        {
+            healthText.text = health.ToString();
+        }
+    }
+}
