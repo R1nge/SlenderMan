5cb33f374ffbe6df6d22185027a5d5577a7acec6
R1nge <r1ngegamedev@gmail.com>
2023-08-11 20:17:33 +0300

    Human death sound

---
+++ Assets/Scripts/Characters/Human/HumanSounds.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Characters.Human
+{
+    public class HumanSounds : NetworkBehaviour
+    {
+        [SerializeField] private AudioSource deathSoundPrefab;
+        private HumanHealthView _humanHealth;
+
+        private void Start()
+        {
+            if (!IsOwner) return;
+            _humanHealth = GetComponent<HumanHealthView>();
+            _humanHealth.IsDead.OnValueChanged += OnDeath;
+        }
+
+        private void OnDeath(bool _, bool isDead)
+        {
+            SpawnDeathSoundServerRpc();
+        }
+
+        [ServerRpc]
+        private void SpawnDeathSoundServerRpc()
+        {
+            var sound = Instantiate(deathSoundPrefab, transform.position, Quaternion.identity);
+            sound.GetComponent<NetworkObject>().Spawn(true);
+        }
+    }
+}
