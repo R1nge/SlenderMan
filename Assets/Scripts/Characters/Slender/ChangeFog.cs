84238ca9da966ecb7fb8fadbec216b68eea1c266
R1nge <r1ngegamedev@gmail.com>
2023-08-11 13:27:06 +0300

    Disabled fog for slender

---
+++ Assets/Scripts/Characters/Slender/ChangeFog.cs
@@ -0,0 +1,26 @@
+﻿using Meryuhi.Rendering;
+using Unity.Netcode;
+using UnityEngine.Rendering;
+
+namespace Characters.Slender
+{
+    public class ChangeFog : NetworkBehaviour
+    {
+        private Volume _volume;
+
+        private void Start()
+        {
+            print($"Is local {IsOwner && IsLocalPlayer && NetworkObject.IsOwner && NetworkObject.IsLocalPlayer}");
+            if (!IsOwner && !IsLocalPlayer && !NetworkObject.IsOwner && !NetworkObject.IsLocalPlayer) return;
+            _volume = FindObjectOfType<Volume>();
+            //_volume.enabled = false;
+            var vol = _volume.profile;
+            
+            if (vol.TryGet(out FullScreenFog fog))
+            {
+                fog.intensity.Override(0);
+                fog.density.Override(0);
+            }
+        }
+    }
+}
