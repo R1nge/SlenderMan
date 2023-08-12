d93807dd8e80a513c85b51fc4e1d00468c46304f
R1nge <r1ngegamedev@gmail.com>
2023-08-12 17:39:55 +0300

    Flash light switch

---
+++ Assets/Scripts/Characters/Human/FlashlightController.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Characters.Human
+{
+    public class FlashlightController : NetworkBehaviour
+    {
+        [SerializeField] private Light light;
+        private bool _enabled;
+
+        private void Awake()
+        {
+            _enabled = true;
+        }
+
+        private void Update()
+        {
+            if (!IsOwner) return;
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                SwitchServerRpc();
+            }
+        }
+
+        [ServerRpc]
+        private void SwitchServerRpc()
+        {
+            _enabled = !_enabled;
+            light.enabled = _enabled;
+            SwitchClientRpc(_enabled);
+        }
+
+        [ClientRpc]
+        private void SwitchClientRpc(bool enabled)
+        {
+            if (IsServer) return;
+            light.enabled = enabled;
+        }
+    }
+}
