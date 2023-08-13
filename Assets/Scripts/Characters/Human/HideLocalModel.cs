5398a95da25ee6c4853dce31082da76d884bfb46
R1nge <r1ngegamedev@gmail.com>
2023-08-13 11:06:37 +0300

    Hid local model

---
+++ Assets/Scripts/Characters/Human/HideLocalModel.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Characters.Human
+{
+    public class HideLocalModel : NetworkBehaviour
+    {
+        [SerializeField] private Renderer renderer;
+
+        private void Start()
+        {
+            if (!IsOwner) return;
+            renderer.enabled = false;
+        }
+    }
+}
